9036a270a893b9ec0d1d58cca5a1eb49424a8823
Mark Needham <mark.needham@neotechnology.com>
2018-04-03 04:38:31 +0100

    simplify setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Setup script for networkx
-You can install networkx with
+Setup script for networkx-neo4j
+You can install networkx-neo4j with
 python setup.py install
 """
-from glob import glob
 import os
 import sys
+
 if os.path.exists('MANIFEST'):
     os.remove('MANIFEST')
 
@@ -22,40 +22,22 @@ if sys.version_info[:2] < (3, 6):
           sys.version_info[:2])
     sys.exit(-1)
 
-# Write the version information.
-sys.path.insert(0, 'networkx-neo4j')
-import release
-version = release.write_versionfile()
-sys.path.pop(0)
-
 packages = ["nxneo4j"]
 
-docdirbase = 'share/doc/networkx-neo4j-%s' % version
-# add basic documentation
-data = [(docdirbase, glob("*.txt"))]
-# add examples
-
-
 if __name__ == "__main__":
-
     setup(
-        name=release.name.lower(),
-        version=version,
-        maintainer=release.maintainer,
-        maintainer_email=release.maintainer_email,
-        author=release.authors['Hagberg'][0],
-        author_email=release.authors['Hagberg'][1],
-        description=release.description,
-        keywords=release.keywords,
-        long_description=release.long_description,
-        license=release.license,
-        platforms=release.platforms,
-        url=release.url,
-        download_url=release.download_url,
-        classifiers=release.classifiers,
+        name="networkx-neo4j",
+        version="0.0.1",
+        maintainer="Mark Needham",
+        maintainer_email="m.h.needham@gmal.com",
+        author="Mark Needham",
+        author_email="m.h.needham@gmal.com",
+        description="NetworkX API for Neo4j Graph Algorithms",
+        keywords="neo4j, networkx",
+        long_description="NetworkX API for Neo4j Graph Algorithms",
+        license="Apache 2",
+        platforms="All",
+        url="http://markhneedham.com",
         packages=packages,
-        data_files=data,
-        test_suite='nose.collector',
-        tests_require=['nose>=0.10.1'],
         zip_safe=False
     )
