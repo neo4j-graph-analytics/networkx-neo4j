60b379662eddea10fa5e9f97a4a700914492838a
Mark Needham <mark.needham@neotechnology.com>
2018-04-04 08:15:21 +0100

    support container of nodes

--- nxneo4j/community.py
+++ nxneo4j/community.py
@@ -1,6 +1,17 @@
 def triangles(G, nodes=None):
-    return G.triangles()
+    triangles = G.triangles()
+
+    if nodes:
+        return {k: v for k, v in triangles.items() if k in nodes}
+
+    return triangles
 
 
 def clustering(G, nodes=None, weight=None):
+    # doesn't currently support `weight`
+    triangles = G.triangles()
+
+    if nodes:
+        return {k: v for k, v in triangles.items() if k in nodes}
+
     return G.clustering()
