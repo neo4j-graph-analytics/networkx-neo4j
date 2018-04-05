fcbd70152b695286823fc9dc0df2d95b3a37bc22
Mark Needham <mark.needham@neotechnology.com>
2018-04-05 11:42:08 +0100

    parameterise everywhere

--- nxneo4j/graph.py
+++ nxneo4j/graph.py
@@ -8,7 +8,7 @@ class Graph:
         self.node_label = config.get("node_label", "Node")
         self.relationship_type = config.get("relationship_type", "CONNECTED")
         self.graph = config.get("graph", "heavy")
-        self.identifier_property = config.get("identifier_property", "value")
+        self.identifier_property = config.get("identifier_property", "id")
 
     add_node_query = """\
     MERGE (:`%s` {`%s`: {value} })
@@ -81,12 +81,12 @@ class Graph:
     })
     YIELD nodeId, centrality
     MATCH (n) WHERE id(n) = nodeId
-    RETURN n.value AS node, centrality
+    RETURN n.`%s` AS node, centrality
     """
 
     def betweenness_centrality(self):
         with self.driver.session() as session:
-            query = self.betweenness_centrality_query
+            query = self.betweenness_centrality_query % self.identifier_property
             params = self.base_params()
             result = {row["node"]: row["centrality"] for row in session.run(query, params)}
         return result
@@ -99,14 +99,14 @@ class Graph:
     })
     YIELD nodeId, centrality
     MATCH (n) WHERE id(n) = nodeId
-    RETURN n.value AS node, centrality
+    RETURN n.`%s` AS node, centrality
     """
 
     def closeness_centrality(self, wf_improved=True):
         with self.driver.session() as session:
             params = self.base_params()
             params["wfImproved"] = wf_improved
-            query = self.closeness_centrality_query
+            query = self.closeness_centrality_query % self.identifier_property
             result = {row["node"]: row["centrality"] for row in session.run(query, params)}
         return result
 
@@ -117,7 +117,7 @@ class Graph:
     })
     YIELD nodeId, centrality
     MATCH (n) WHERE id(n) = nodeId
-    RETURN n.value AS node, centrality
+    RETURN n.`%s` AS node, centrality
     """
 
     def harmonic_centrality(self):
@@ -135,8 +135,8 @@ class Graph:
       dampingFactor: {dampingFactor}
     })
     YIELD nodeId, score
-    MATCH (n:`%s`) WHERE id(n) = nodeId
-    RETURN n.value AS node, score
+    MATCH (n) WHERE id(n) = nodeId
+    RETURN n.`%s` AS node, score
     """
 
     def pagerank(self, alpha, max_iter):
@@ -156,20 +156,20 @@ class Graph:
     })
     YIELD nodeId, triangles, coefficient
     MATCH (n) WHERE id(n) = nodeId
-    RETURN n.value AS node, triangles, coefficient
+    RETURN n.`%s` AS node, triangles, coefficient
     """
 
     def triangles(self):
         with self.driver.session() as session:
             params = self.base_params()
-            query = self.triangle_count_query
+            query = self.triangle_count_query % self.identifier_property
             result = {row["node"]: row["triangles"] for row in session.run(query, params)}
         return result
 
     def clustering(self):
         with self.driver.session() as session:
             params = self.base_params()
-            query = self.triangle_count_query
+            query = self.triangle_count_query % self.identifier_property
             result = {row["node"]: row["coefficient"] for row in session.run(query, params)}
         return result
 
@@ -195,13 +195,13 @@ class Graph:
     })
     YIELD nodeId, label
     MATCH (n) WHERE id(n) = nodeId
-    RETURN label, collect(n.value) AS nodes
+    RETURN label, collect(n.`%s`) AS nodes
     """
 
     def label_propagation(self):
         with self.driver.session() as session:
             params = self.base_params()
-            query = self.lpa_query
+            query = self.lpa_query % self.identifier_property
 
             for row in session.run(query, params):
                 yield set(row["nodes"])
