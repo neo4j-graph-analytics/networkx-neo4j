f06b7fb789d1bc9cdc861eaa9a5211a986056294
Yusuf Baktir <baktiryusuf@gmail.com>
2020-05-09 05:39:57 -0500

    load_got, load_euroads, load_twitter were added, updated example ipynb

--- nxneo4j/base_graph.py
+++ nxneo4j/base_graph.py
@@ -268,3 +268,94 @@ class BaseGraph:
 
             result = [row["node"] for row in session.run(query, params)]
         return result
+
+    def delete_all(self):
+        with self.driver.session() as session:
+            session.run("MATCH (n) DETACH DELETE n")
+
+    def load_got(self):
+        """
+        Author: Andrew Beveridge
+        https://twitter.com/mathbeveridge
+        """
+        with self.driver.session() as session:
+            session.run("""\
+            CREATE CONSTRAINT ON (c:Character)
+            ASSERT c.name IS UNIQUE
+            """)
+
+            session.run("""\
+            LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/mathbeveridge/asoiaf/master/data/asoiaf-book1-edges.csv" AS row
+            MERGE (src:Character {name: row.Source})
+            MERGE (tgt:Character {name: row.Target})
+            // relationship for the book
+            MERGE (src)-[r:INTERACTS1]->(tgt)
+            ON CREATE SET r.weight = toInt(row.weight), r.book=1
+            """)
+
+            session.run("""\
+            LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/mathbeveridge/asoiaf/master/data/asoiaf-book2-edges.csv" AS row
+            MERGE (src:Character {name: row.Source})
+            MERGE (tgt:Character {name: row.Target})
+            // relationship for the book
+            MERGE (src)-[r:INTERACTS2]->(tgt)
+            ON CREATE SET r.weight = toInt(row.weight), r.book=2
+            """)
+
+            session.run("""\
+            LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/mathbeveridge/asoiaf/master/data/asoiaf-book3-edges.csv" AS row
+            MERGE (src:Character {name: row.Source})
+            MERGE (tgt:Character {name: row.Target})
+            // relationship for the book
+            MERGE (src)-[r:INTERACTS3]->(tgt)
+            ON CREATE SET r.weight = toInt(row.weight), r.book=3
+            """)
+
+            session.run("""\
+            LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/mathbeveridge/asoiaf/master/data/asoiaf-book45-edges.csv" AS row
+            MERGE (src:Character {name: row.Source})
+            MERGE (tgt:Character {name: row.Target})
+            // relationship for the book
+            MERGE (src)-[r:INTERACTS45]->(tgt)
+            ON CREATE SET r.weight = toInt(row.weight), r.book=45
+            """)
+
+    def load_euroads(self):
+        with self.driver.session() as session:
+            session.run("""\
+            CREATE CONSTRAINT ON (p:Place) ASSERT p.name IS UNIQUE
+            """)
+
+            session.run("""\
+            USING PERIODIC COMMIT 1000
+            LOAD CSV WITH HEADERS FROM "https://github.com/neo4j-apps/neuler/raw/master/sample-data/eroads/roads.csv"
+            AS row
+
+            MERGE (origin:Place {name: row.origin_reference_place})
+            SET origin.countryCode = row.origin_country_code
+
+            MERGE (destination:Place {name: row.destination_reference_place})
+            SET destination.countryCode = row.destination_country_code
+
+            MERGE (origin)-[eroad:EROAD {road_number: row.road_number}]->(destination)
+            SET eroad.distance = toInteger(row.distance), eroad.watercrossing = row.watercrossing
+            """)
+
+    def load_twitter(self):
+        with self.driver.session() as session:
+            session.run("""\
+            CREATE CONSTRAINT ON(u:User) ASSERT u.id IS unique
+            """)
+
+            session.run("""\
+            CALL apoc.load.json("https://github.com/neo4j-apps/neuler/raw/master/sample-data/twitter/users.json")
+            YIELD value
+            MERGE (u:User {id: value.user.id })
+            SET u += value.user
+            FOREACH (following IN value.following |
+              MERGE (f1:User {id: following})
+              MERGE (u)-[:FOLLOWS]->(f1))
+            FOREACH (follower IN value.followers |
+              MERGE(f2:User {id: follower})
+              MERGE (u)<-[:FOLLOWS]-(f2));
+            """)
